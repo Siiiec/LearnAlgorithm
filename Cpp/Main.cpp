2df656407554f425a99ebc46c0b5c5d79bc61b74
Siiiec <20ca39mg@gmail.com>
2018-04-16 21:45:52 +0900

    co

--- Cpp/Main.cpp
+++ Cpp/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <functional>
-#include "AOJ\ALDS1\12_GraphII\C_SingleSourceShortestPathII.hpp"
+#include "AOJ\ALDS1\10_DynamicProgramming\A_DP_Fibonacci.hpp"
 
 #include <chrono>
 #include <fstream>
@@ -11,18 +11,8 @@ using namespace std;
 
 int main()
 {
-    ifstream in(string(R"(C:\Users\ca39g\Documents\Visual Studio 2017\Projects\LearnAlgorithm\Release\03.txt)"));
-    cin.rdbuf(in.rdbuf());
-
-    auto begin = high_resolution_clock::now();
 
     solve();
-
-    auto end = high_resolution_clock::now();
-
-    auto time =  duration_cast<milliseconds>(end - begin);
-    
-    std::cout << time.count() << std::endl;
 
     return 0;
 
