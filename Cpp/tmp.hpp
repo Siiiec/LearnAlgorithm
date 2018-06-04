9cc68f906f65a0632a7656500f748d3a5bee272d
Siiiec <20ca39mg@gmail.com>
2018-06-04 16:10:38 +0900

    tmp

--- Cpp/tmp.hpp
+++ Cpp/tmp.hpp
@@ -25,90 +25,15 @@ using vec = std::vector<T>;
 void solve()
 {
     using namespace std;
-    int n;
-    cin >> n;
+    
+    string str;
+    cin >> str;
 
-    constexpr int female = 0;
-    constexpr int male = 1;
-
-    constexpr int notMatched = -1;
-    constexpr int matched = 10;
-
-    vec<vec<int>> match(2, vec<int>(n, notMatched));
-
-    vec<vec<int>> frank(n);
-    vec<vec<int>> mrank(n);
-    vec<int> ans(n);
-
-    //“ü—Í(—«)
-    for (auto& v : frank)
+    for (int i = 1; i < str.size() - 1; ++i)
     {
-        for (int i = 0; i < n; ++i)
-        {
-            int in;
-            cin >> in;
-            v.push_back(in - 1);
-        }
-    }
-    //“ü—Í(’j«)
-    for (auto& v : mrank)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            int in;
-            cin >> in;
-            v.push_back(in - 1);
-        }
-    }
-
-    for (int i = 0; i < n; ++i)
-    {
-        set<string> se;
-        //FtoM
-        for (int f = 0; f < n; ++f)
-        {
-            if (match[female][f] == matched)
-                continue;
-            for (int j = 0; j < n; ++j)
-            {
-                int m = frank[f][j];
-                if (match[male][m] != matched)
-                {
-                    se.emplace(to_string(f) + to_string(m));
-                    break;
-                }
-            }
-        }
-
-        //MtoF
-        for (int m = 0; m < n; ++m)
-        {
-            if (match[male][m] == matched)
-                continue;
-            for (int j = 0; j < n; ++j)
-            {
-                int f = mrank[m][j];
-                if (match[female][f] != matched)
-                {
-                    auto matchStr = to_string(f) + to_string(m);
-                    if (se.count(matchStr) != 0)
-                    {
-                        match[male][m] = matched;
-                        match[female][f] = matched;
-                        ans[f] = m + 1;
-                        break;
-                    }
-
-                }
-            }
-        }
 
     }
 
-
-
-    for (auto m : ans)
-        cout << m << endl;
 }
 
 //int main()
