02cf0d7586015131fe29a9bc1698b19170bcfcc3
Siiiec <20ca39mg@gmail.com>
2018-06-08 13:55:17 +0900

    tmp

--- Cpp/tmp.hpp
+++ Cpp/tmp.hpp
@@ -26,11 +26,50 @@ using vec = std::vector<T>;
 void solve()
 {
     using namespace std;
-    ll X;
-    cin >> X;
-    double n = ceil(0.5 * (-1 + sqrt(1 + 8 * X)));
-    cout << (ll)(n) << endl;
+    
+    constexpr int maxK = 5000;
+    
+    int N, K;
+    cin >> N >> K;
 
+    vec<int> a(N);
+    for (auto& x : a) cin >> x;
+    sort(a.begin(), a.end());
+
+    //dp[j + 1][j]jî‘ñ⁄Ç‹Ç≈Ç≈íljÇçÏÇÍÇÈÇ©Ç«Ç§Ç©
+    vec<vec<int>> dp(N + 1, vec<int>(maxK + 1, 100000));
+
+    for (int i = 0; i <= N; ++i)
+        dp[i][0] = 0;
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j <= maxK; ++j)
+        {
+            dp[i + 1][j] = dp[i][j];
+            if (j >= a[i])
+                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j - a[i]] + 1);
+        }
+    
+    vec<int> v;
+
+    for (int j = 0; j < dp[N].size(); ++j)
+        if (dp[N][j] != 100000)
+            v.push_back(j);
+
+    int count {};
+    for (int i = 0; i < N; ++i)
+    {
+        bool important = any_of(v.cbegin(), v.cend(), 
+            [K, &a, i](int x)
+        {
+            return x >= K && x < K + a[i];
+        });
+
+        if (!important)
+            ++count;
+    }
+
+    cout << count << endl;
 }
 
 //int main()
