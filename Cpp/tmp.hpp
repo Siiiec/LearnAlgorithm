4c01c69461fefb5632a185939e0745ecc75e57cc
Siiiec <20ca39mg@gmail.com>
2018-06-16 22:15:09 +0900

    ABC100

--- Cpp/tmp.hpp
+++ Cpp/tmp.hpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -38,6 +39,7 @@ namespace fs = std::filesystem;
 #endif
 
 namespace {
+
     using ll = long long;
     using ull = unsigned long long;
 
@@ -50,8 +52,17 @@ namespace {
 
     constexpr std::size_t operator""_sz(ull n) { return std::size_t (n); }
 
+    //template <class Container>
+    //std::istream& operator>>(std::istream& is, Container& c)
+    //{
+    //    const auto end = std::cend(c);
+    //    for (auto iter = std::cbegin(c); iter != end; ++iter)
+    //        is >> *iter;
+    //    return is;
+    //}
+    
     template <class T, class BinaryOperation>
-    constexpr T foldr(std::initializer_list<T> args, T init, BinaryOperation op)
+    constexpr T fold(std::initializer_list<T> args, T init, BinaryOperation op)
     {
         return std::accumulate(args.begin(), args.end(), init, op);
     }
@@ -62,134 +73,6 @@ namespace {
 #endif
     ///////////////////////////////////////////////////////////
     // Vector
-    
-    template <class T>
-    struct Vec2D
-    {
-        T x, y;
-
-        Vec2D() = default;
-
-        constexpr Vec2D(T x, T y)
-            : x {x}, y {y}
-        {}
-
-        static constexpr T Dot(const Vec2D& lhs, const Vec2D& rhs)
-        {
-            return lhs.x * rhs.x + lhs.y * rhs.y;
-        }
-
-        static constexpr T Cross(const Vec2D& lhs, const Vec2D& rhs)
-        {
-            return {lhs.x*rhs.y - lhs.y*rhs.x};
-        }
-
-        constexpr T dot(const Vec2D& rhs) const
-        {
-            Dot(*this, rhs);
-        }
-
-        constexpr Vec2D cross(const Vec2D& rhs) const
-        {
-            Dot(*this, rhs);
-        }
-
-        T magnitude() const
-        {
-            return std::sqrt(sqrMagnitude());
-        }
-
-        constexpr T sqrMagnitude() const
-        {
-            return x * x + y * y;
-        }
-
-        Vec2D normalized() const
-        {
-            return *this / magnitude();
-        }
-
-        Vec2D normalize()
-        {
-            *this = normalized();
-            return *this;
-        }
-
-        constexpr bool isZero() const
-        {
-            return x == 0 && y == 0;
-        }
-
-        constexpr Vec2D operator+() const
-        {
-            return *this;
-        }
-
-        constexpr Vec2D operator-() const
-        {
-            return {-x, -y};
-        }
-
-
-        constexpr Vec2D operator+(Vec2D rhs) const
-        {
-            return {x + rhs.x, y + rhs.y};
-        }
-
-        constexpr Vec2D operator-(Vec2D rhs) const
-        {
-            return {x - rhs.x, y - rhs.y};
-        }
-
-        template <class U>
-        constexpr Vec2D operator*(U rhs) const
-        {
-            return {x * rhs, y * rhs};
-        }
-
-        template <class U>
-        constexpr Vec2D operator/(U rhs) const
-        {
-            return {x / rhs, y / rhs};
-        }
-
-        constexpr Vec2D& operator+=(const Vec2D& other)
-        {
-            x += other.x;
-            y += other.y;
-            return *this;
-        }
-
-        constexpr Vec2D& operator-=(const Vec2D& other)
-        {
-            x -= other.x;
-            y -= other.y;
-            return *this;
-        }
-
-        template <class U>
-        constexpr Vec2D& operator*=(U s)
-        {
-            x *= s;
-            y *= s;
-            return *this;
-        }
-
-        template <class U>
-        constexpr Vec2D& operator/=(U s)
-        {
-            x /= s;
-            y /= s;
-            return *this;
-        }
-
-    };
-
-    template<class T, class U>
-    inline constexpr Vec2D<T> operator*(U s, Vec2D<T> v)
-    {
-        return {s * v.x, s * v.y};
-    }
 
     template <class T>
     struct Vec3D
@@ -339,10 +222,10 @@ namespace {
 
     // argsからハッシュ値を作成
     template <class... Args>
-    size_t hash(Args... args)
+    std::size_t hash(Args... args)
     {
-        return foldr({static_cast<size_t>(args)...}, 0_sz,
-            [](auto seed, auto x)
+        return fold({static_cast<std::size_t>(args)...}, 0_sz,
+            [](std::size_t seed, std::size_t x)
             {
                 // uses magic number from boost
                 return seed ^= x + 0x9e3779b9 + (seed << 6) + (seed >> 2);
@@ -372,72 +255,19 @@ namespace {
         printAll(ini.begin(), ini.end(), delimiter);
     }
 
+    struct Eval
+    {
+        ll x, y, z;
+
+        ll sum() const
+        {
+            return abs(x) + abs(y) + abs(z);
+        }
+    };
+
     void solve()
     {
-        using namespace std;
-
-        //int N, K;
-        //cin >> N >> K;
-
-        //vec<int> a(N);
-        //for (auto& x : a)
-        //    cin >> x;
-        //
-        //// dp[i + 1][j] 左からi番目まで使用してjが作れるか
-        //vv<bool> dpl(N + 1, vec<bool>(K + 1, false));
-        //// dp[i + 1][j] (a[i], a[N - 1]]まで使用してjが作れるか
-        //vv<bool> dpr(N + 1, vec<bool>(K + 1, false));
-
-        //for (int i = 0; i <= N; ++i)
-        //{
-        //    dpl[i][0] = true;
-        //}
-
-        //// 左からDP
-        //for (int i = 0; i < N; ++i)
-        //    for (int j = 0; j <= K; ++j)
-        //    {
-        //        dpl[i + 1][j] = dpl[i + 1][j] | dpl[i][j];
-        //        if (j >= a[i])
-        //            dpl[i + 1][j] = dpl[i + 1][j] | dpl[i][j - a[i]];
-        //    }
-
-        //for (int i = 0; i <= N; ++i)
-        //{
-        //    dpr[i][0] = true;
-        //}
-
-        //// 右からDP
-        //for (int i = N; i > 0; --i)
-        //    for (int j = 0; j <= K; ++j)
-        //    {
-        //        dpr[i - 1][j] = dpr[i - 1][j] | dpr[i][j];
-        //        if (j >= a[i - 1])
-        //            dpr[i - 1][j] = dpr[i - 1][j] | dpr[i][j - a[i - 1]];
-        //    }
-
-        //cout << ::hash(1, 3);
-
-        //int no_needed {};
-
-        Vec2D<double> x {1, 0};
-        Vec2D<double> y {0, 1};
-
-        auto xy = Vec2D<double>::Cross(x, y);
-
-
-        auto printVec = [](Vec2D<double> v)
-        {
-            printAll({v.x, v.y}, " ");
-            cout << endl;
-        };
-
-        printVec(x * 10);
-        printVec(10 * x);
-
-        printVec(x / 2.0);
-        printVec(x - 4.0 * y);
-
+        
 
     }
 }
