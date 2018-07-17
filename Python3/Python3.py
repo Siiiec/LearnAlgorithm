73e7978bcfb926da76b8b32b99c474af3e18997e
Siiiec <20ca39mg@gmail.com>
2018-07-17 20:34:52 +0900

    com

--- Python3/Python3.py
+++ Python3/Python3.py
@@ -1,4 +1,30 @@
+import math
+
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
+t = input()
+
+s = input()
+sl = s.split(" ")
+
+even = 0
+odd = 0
+
+for si in sl:
+    if int(si[-1]) % 2 == 0:
+        even += 1
+    else:
+        odd +=1
+
+combi = [i for i in range(odd + 1) if i % 2 == 0]
+
+a = 0
+
+for c in combi:
+    a += nCr(odd, c)
 
 
-
-
+b = a * pow(2, even)
+print(int(b - 1))
